68f0a51c6094ae05351203c6f973f55850539486
Triforcedrake <lhansenjacob@gmail.com>
2021-01-11 13:01:29 +0100

    First version

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { Ionicons, FontAwesome5, Entypo } from '@expo/vector-icons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+import SwipeButton from 'rn-swipe-button';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: 'Unlock Freetrailer',
+      icon: <FontAwesome5 name="lock" size={100} color="black" />,
+    };
+  }
+
+  
+  showIcon = icon => this.setState({icon}); 
+
+  renderIconStatus = () => (
+      <Text style={styles.text}>{this.state.icon}</Text>
   );
+
+  showMessage = message => this.setState({message});
+
+  renderMessageStatus = () => (
+    <Text style={styles.text}>{this.state.message}</Text>
+  );
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ImageBackground source={require('./assets/img/background.jpg')} style={styles.background}>
+          {this.renderIconStatus()}
+          {this.renderMessageStatus()}
+          <SwipeButton
+            RightToLeftSwipe
+            thumbIconBackgroundColor="#FFFFFF"
+            title=""
+            swipeSuccessThreshold={70}
+            railFillBackgroundColor="Blue"
+            onSwipeStart={() => {this.showMessage('Opening Freetrailer'); this.showIcon(<Entypo name="progress-two" size={100} color="black" />);}}
+            onSwipeSuccess={() => {this.showMessage('Your Freetrailer is now open'); this.showIcon(<Ionicons name="md-checkmark-circle" size={100} color="green" />);}}
+          />
+        </ImageBackground>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  background: {
+      flex: 1,
+      resizeMode: "cover",
+      justifyContent: "center"
+  },
+  text: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: "bold",
+    textAlign: "center",
+},
+
 });
