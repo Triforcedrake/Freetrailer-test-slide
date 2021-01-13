cbd5db8ea37a0ca5cf1d8b57c0554815fed93882
Triforcedrake <lhansenjacob@gmail.com>
2021-01-13 09:53:18 +0100

    Styling

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-import { Ionicons, FontAwesome5, Entypo } from '@expo/vector-icons';
+import { Text, View, ImageBackground } from 'react-native';
+import { Ionicons, Entypo } from '@expo/vector-icons';
+import StyleSheet from './components/my_styles'; 
 
 import SwipeButton from 'rn-swipe-button';
 
@@ -8,60 +9,54 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: 'Unlock Freetrailer',
-      icon: <FontAwesome5 name="lock" size={100} color="black" />,
+      message: 'Unlock\nFreetrailer',
+      icon: <Ionicons name="lock-closed-outline" size={100} color="white" />,
     };
   }
 
-  
+  keyIcon = () => <Ionicons name="key-outline" size={40} color="white" /> 
+
   showIcon = icon => this.setState({icon}); 
 
   renderIconStatus = () => (
-      <Text style={styles.text}>{this.state.icon}</Text>
+      <Text style={StyleSheet.text}>{this.state.icon}</Text>
   );
 
   showMessage = message => this.setState({message});
 
   renderMessageStatus = () => (
-    <Text style={styles.text}>{this.state.message}</Text>
+    <Text style={StyleSheet.text}>{this.state.message}</Text>
   );
 
   render() {
     return (
-      <View style={styles.container}>
-        <ImageBackground source={require('./assets/img/background.jpg')} style={styles.background}>
-          {this.renderIconStatus()}
-          {this.renderMessageStatus()}
+      <View style={StyleSheet.container}>
+        <ImageBackground source={require('./assets/img/background.jpg')} style={StyleSheet.background}>
+          <View style={StyleSheet.top}>
+            {this.renderIconStatus()}
+            {this.renderMessageStatus()}
+          </View>
+          <View style={StyleSheet.swipe}>
+            <Text style={[StyleSheet.text, StyleSheet.lowerCase]}>Slide to open</Text>
           <SwipeButton
             RightToLeftSwipe
-            thumbIconBackgroundColor="#FFFFFF"
+            thumbIconComponent={this.keyIcon}
+            thumbIconBackgroundColor="teal"
             title=""
             swipeSuccessThreshold={70}
-            railFillBackgroundColor="Blue"
-            onSwipeStart={() => {this.showMessage('Opening Freetrailer'); this.showIcon(<Entypo name="progress-two" size={100} color="black" />);}}
-            onSwipeSuccess={() => {this.showMessage('Your Freetrailer is now open'); this.showIcon(<Ionicons name="md-checkmark-circle" size={100} color="green" />);}}
-          />
+            railFillBackgroundColor="transparent"
+            railFillBorderColor="transparent"
+            onSwipeStart={() => 
+              {this.showMessage('Opening\nFreetrailer'); 
+              this.showIcon(<Entypo name="progress-two" size={100} color="white" />);
+            }}
+            onSwipeSuccess={() => {
+              this.showMessage('Your Freetrailer is now open'); 
+              this.showIcon(<Ionicons name="md-checkmark-circle" size={100} color="green" />);
+            }}
+          /></View>
         </ImageBackground>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-
-  background: {
-      flex: 1,
-      resizeMode: "cover",
-      justifyContent: "center"
-  },
-  text: {
-    fontSize: 20,
-    color: 'white',
-    fontWeight: "bold",
-    textAlign: "center",
-},
-
-});
---
+++ components/my_styles.js
@@ -0,0 +1,44 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+  
+    background: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "space-around",
+        flexDirection: "column"
+    },
+
+    top: {
+        flex: 4, 
+        paddingTop: 200, 
+    },
+
+    swipe: {
+        flex: 2, 
+        padding: 30,
+    },
+
+    center: {
+        textAlign: "center",
+    },
+    
+    text: {
+      fontSize: 40,
+      paddingTop: 20, 
+      paddingLeft: 20,
+      paddingRight: 20,  
+      color: "white",
+      fontWeight: "bold",
+      textAlign: "center",
+    },
+
+    lowerCase: {
+        fontSize: 20, 
+        fontWeight: "normal",
+    },   
+
+});   
