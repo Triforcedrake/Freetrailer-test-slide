457aa8116d374e1f99fa0d3f5dd724083d897fbd
Triforcedrake <lhansenjacob@gmail.com>
2021-01-14 10:00:34 +0100

    Final changes

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Text, View, ImageBackground, Image } from 'react-native';
-import { Ionicons, Entypo } from '@expo/vector-icons';
-import StyleSheet from './components/my_styles'; 
+import React from "react";
+import { Text, View, ImageBackground, Image } from "react-native";
+import { Ionicons, Entypo } from "@expo/vector-icons";
+import StyleSheet from "./components/my_styles"; 
 
-import SwipeButton from 'rn-swipe-button';
+import SwipeButton from "rn-swipe-button";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: 'Unlock\nFreetrailer',
+      message: "Unlock\nFreetrailer",
       icon: <Ionicons name="lock-closed-outline" size={100} color="white" />,
     };
   }
@@ -31,7 +31,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={StyleSheet.container}>
-        <ImageBackground source={require('./assets/img/background.jpg')} style={StyleSheet.background}>
+        <ImageBackground source={require("./assets/img/background.jpg")} style={StyleSheet.background}>
           <View style={StyleSheet.top}>
             {this.renderIconStatus()}
             {this.renderMessageStatus()}
@@ -48,12 +48,12 @@ export default class App extends React.Component {
             railFillBackgroundColor="transparent"
             railFillBorderColor="transparent"
             onSwipeStart={() => 
-              {this.showMessage('Opening\nFreetrailer'); 
+              {this.showMessage("Opening\nFreetrailer"); 
               this.showIcon(<Entypo name="progress-two" size={100} color="white" />);
             }}
             onSwipeSuccess={() => {
-              this.showMessage('Your Freetrailer is now open'); 
-              this.showIcon(<Ionicons name="md-checkmark-circle" size={100} color="green" />);
+              this.showMessage("Your Freetrailer is now open"); 
+              this.showIcon(<Ionicons name="md-checkmark-circle" size={100} color="lightgreen" />);
             }}
           /></View>
         </ImageBackground>
